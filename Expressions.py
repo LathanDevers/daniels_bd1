3e9f38471bac33ca8ad33b65118d93a344a6ab40
Maxime <maxime.daniels@outlook.com>
2016-12-13 22:08:20 +0100

    Select-Project
    Recuperation des inputs de l'utilisateur et transformation
    en requete sql pour select et project

--- Expressions.py
+++ Expressions.py
@@ -36,16 +36,11 @@ class Const(Expr):
 #EXPRESSIONS
 class Select(Expr):
     def __init__(self, expr1, expr2, expr3):
-        if (expr3.sort()==ALL or str(expr1) in expr3.sort()) and expr1.getType()==expr2.getType():
-            Expr.__init__(self, expr1)
-            self.expr2=expr2
-            self.expr3=expr3
-        elif expr1.getType()!=expr2.getType():
-            print("error types : {} is {} and {} is {}".format(expr1, expr1.getType(), expr2, expr2.getType()))
-        else:
-            print("{} not in {}".format(expr1, expr2))
+        Expr.__init__(self, expr1)
+        self.expr2=expr2
+        self.expr3=expr3
     def __str__(self):
-        return "(Select * from {} where {}={})".format(self.expr3, self.expr1, self.expr2)
+        return "(select * from {} where {}={})".format(self.expr3, self.expr1, self.expr2)
     def sort(self):
         return self.expr3.sort()
 
@@ -54,10 +49,10 @@ class Project(Expr):
         Expr.__init__(self, expr1)
         self.attrs=attrs
     def __str__(self):
-        getStr="(select "+str(self.attrs[0])+", "
-        for i in range(1, len(self.attrs)):
+        getStr="(select "
+        for i in range(len(self.attrs)-1):
             getStr+=str(self.attrs[i])+", "
-        getStr+=" from {}) ".format(self.expr1)
+        getStr+="{} from {})".format(self.attrs[-1], self.expr1)
         return getStr
     def sort(self):
         sortL=[]
@@ -67,7 +62,10 @@ class Project(Expr):
 
 class Join(Expr):
     def __init__(self, expr1, expr2):
-        pass
+        Expr.__init__(self, expr1)
+        self.expr2=expr2
+    def __str__(self):
+        return "("+str(self.expr1)+" union "+str(self.expr2)+")"
 
 class Rename(Expr):
     pass
--- Main.py
+++ Main.py
@@ -4,14 +4,57 @@ from Expressions import Select
 from Expressions import Rel
 from Expressions import Const
 from Expressions import Project
+from Expressions import Join
 import sqlite3
 
-print(Select(Attr("A1"), Const("Charles"), Rel("R1")))
+"""print(Select(Attr("A1"), Const("Charles"), Rel("R1")))
 print(Project([Attr("A1"), Attr("A2")], Select(Attr("A1"), Const("Charles"), Rel("R1"))))
-#print(Select(Attr("A1"), Const("Charles"), Project([Attr("A2")], Rel("R1"))))
+print(Select(Attr("A1"), Const("Charles"), Project([Attr("A2")], Rel("R1"))))
 print(Project([Attr("A1")], Rel("R1")).sort())
 print(Select(Attr("A1"), Const("Charles"), Rel("R1")).sort())
-print("\n")
+print("\n")"""
+
+def printage():
+    print("Base de donnees : R1")
+    for raw in c.execute("select * from R1"):
+        print(raw)
+    print("\n")
+    command=str(Select(Attr("A2"), Const(20), Rel("R1")))[1:-1]
+    print(command)
+    for raw in c.execute(command):
+        print(raw)
+
+def retValue(n, command):
+    for i in range(n, len(command)):
+        if command[i]==")":
+            return command[n:i]
+
+def commands(command):
+    expr=[]
+    atom=[]
+    for i in range(len(command), -1, -1):
+        
+        if command[i:i+5]=="Attr(":
+            value=retValue(i+5, command)
+            atom=[Attr(value)]+atom
+        if command[i:i+4]=="Rel(":
+            value=retValue(i+4, command)
+            expr=[Rel(value)]+expr
+        if command[i:i+6]=="Const(":
+            value=retValue(i+6, command)
+            atom=[Const(value)]+atom
+        if command[i:i+7]=="Select(":
+            expr=[Select(atom[0], atom[1], expr[0])]+expr
+            expr=expr[:-1]
+            atom=[]
+        if command[i:i+8]=="Project(":
+            expr=[Project(atom, expr[0])]+expr
+            expr=expr[:-1]
+            atom=[]
+            
+                
+    print(str(expr[0])[1:-1])
+        
 
 conn=sqlite3.connect('myDB.db')
 c=conn.cursor()
@@ -20,13 +63,16 @@ c.execute('''create table R1(A1 text, A2 float, A3 real)''')
 c.execute("insert into R1 values('Charles', 3, 20)")
 c.execute("insert into R1 values('Maxime', 20, 20)")
 c.execute("insert into R1 values('Guillaume', 3, 5)")
+"""c.execute("select typeof(A1) from R1")
+print(c.fetchone())"""
 
-print("Base de donnees : R1")
-for raw in c.execute("select * from R1"):
-    print(raw)
-print("\n")
-command=str(Select(Attr("A1"), Const(20), Rel("R1")))[1:-1]
-print(str(Attr("A1").getType())+", "+str(Const(20).getType())+"="+str(Attr("A1").getType()==Const(20).getType()))
-print(command)
-for raw in c.execute(command):
-    print(raw)
+def prompter():
+    while(True):
+        command=input("> ")
+        if(command=="exit"):
+            print("goodbye ;)")
+            break
+        commands(command)
+        #printage()
+
+prompter()
